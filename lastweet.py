f17a734b283ef0a396a177d53013693db1b43eaf
Ed Summers <ehs@pobox.com>
2017-09-30 16:17:22 +0000

    added debug

--- lastweet.py
+++ lastweet.py
@@ -17,6 +17,8 @@ import mastodon
 import requests
 import urllib.request
 
+debug = False
+
 # get some environment variables
 e = os.environ.get
 twitter_consumer_key = e('TWITTER_CONSUMER_KEY')
@@ -63,7 +65,7 @@ for artist in artists:
         images.append(image)
 
 # tweet them
-if twitter_consumer_key:
+if not debug and twitter_consumer_key:
     auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
     auth.set_access_token(twitter_access_token, twitter_access_token_secret)
     twitter = tweepy.API(auth)
@@ -74,7 +76,7 @@ if twitter_consumer_key:
         twitter.update_status(msg)
 
 # twoot them
-if mastodon_access_token:
+if not debug and mastodon_access_token:
     m = mastodon.Mastodon(
         client_id=mastodon_client_id,
         client_secret=mastodon_client_secret,
@@ -82,3 +84,8 @@ if mastodon_access_token:
     )
     media_ids = [m.media_post(i) for i in images]
     m.status_post(status=msg, media_ids=media_ids)
+
+if debug:
+    print(msg)
+    for i in images:
+        print("-", i)
